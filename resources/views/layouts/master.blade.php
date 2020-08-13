50d5acc4d3a238848b39f25d597ba7f2166cb5d6
rpratama94 <rpratama.sky@gmail.com>
2020-08-13 16:56:09 +0700

    First Commit - Installation Auth with Admin LTE

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{ asset('/adminlte/plugins/fontawesome-free/css/all.min.css') }} ">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{ asset('/adminlte/plugins/ionicons/2.0.1/css/ionicons.min.css') }}">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="{{ asset('/adminlte/dist/css/adminlte.min.css')}}">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  @stack('styles')
+
+</head>
+<body class="hold-transition sidebar-mini layout-navbar-fixed">
+<!-- Site wrapper -->
+<div class="wrapper">
+
+  <!-- Navbar -->
+  @include('layouts.partials.header')
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  @include('layouts/partials/sidebar')
+  <!-- /.main-side -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>
+              {{ !empty($page) ? $page : 'Page' }}
+            </h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="/">Home</a></li>
+              <li class="breadcrumb-item active">Ini Page</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid-->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Yield Content -->
+
+      @yield('content')
+      
+      <!-- /.conten -->
+
+    </section>
+    <!-- /.content -->
+    
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- footer -->
+  @include('layouts.partials.footer')
+  <!-- /footer -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{ asset('/adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('/adminlte/dist/js/adminlte.min.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('/adminlte/dist/js/demo.js') }}"></script>
+
+<!-- receive push script from other page -->
+@stack('scripts')
+
+</body>
+</html>
