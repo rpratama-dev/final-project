7cae202c6702209eeddb0cd95b85dee9e268c821
rpratama94 <rpratama.sky@gmail.com>
2020-08-14 12:57:05 +0700

    Revisi Route.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index.index');
+    return view('questions.index');
 });
 
 Route::get('/detail', function () {
